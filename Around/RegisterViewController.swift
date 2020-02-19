0e2c7ad28b957af15e7979f9e51a5631ac3526e8
Maor Yadin <maoryadin0888@gmail.com>
2020-02-19 13:47:30 +0200

    created register view controller

--- Around/RegisterViewController.swift
+++ Around/RegisterViewController.swift
@@ -7,24 +7,96 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class RegisterViewController: UIViewController {
 
+    var db:Firestore!
+    var myUser:UserData!
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var ageField: UITextField!
+    
+    @IBOutlet weak var firstNameField: UITextField!
+    
+    @IBOutlet weak var lastNameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+           let settings = FirestoreSettings()
 
-        // Do any additional setup after loading the view.
+          Firestore.firestore().settings = settings
+            // [END setup]
+          db = Firestore.firestore()
     }
     
+    
+    @IBAction func registerOnClick(_ sender: Any) {
+        
+        if(usernameField.text != nil && emailField.text != nil && passwordField.text != nil && ageField.text != nil && firstNameField.text != nil && lastNameField.text != nil){
+            
+            
+            FireBaseManager.CreateAccount(email: emailField.text!, password: passwordField.text!) {
+                        (result:String) in
+                        DispatchQueue.main.async {
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                            self.addUserDetails()
+                            self.performSegue(withIdentifier: "showProfileRegister", sender: sender)
+                        }
+                    }
+        }
+
+        
     }
-    */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.destination is ProfileViewController {
+            
+            let vc = segue.destination as? ProfileViewController
+            vc?.userData = myUser
+        }
+        
+    }
+    
+    @IBAction func backBarItem(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    func addUserDetails() {
+
+        let uid = Auth.auth().currentUser?.uid
+        if uid != nil {
+
+//            let docData: [String: Any] = [
+//              "first": firstNameField.text!,
+//              "last": lastNameField.text!,
+//                     "age":  ageField.text!,
+//                     "email": emailField.text!,
+//                     "id": uid!,
+//                     "profilePicRef":"empty"
+//                     ]
+            
+            myUser = UserData(first: firstNameField.text!, last: lastNameField.text!, profilePic: "", email: emailField.text!, uid: uid!, age: ageField.text!)
+
+                  //   print("enter to add doc func")
+
+
+            db.collection("Users").document(uid!).setData(myUser.toJson(),merge: true)
+            
+
+        }
+        
+    }
+    
 }
