f847231a08598a2671a71fc9dd3d87d22e98ceb8
Maor Yadin <maoryadin0888@gmail.com>
2020-02-29 16:46:04 +0200

    Created a Post class as struct to build as a cell

---
+++ Around/Model/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  Around
+//
+//  Created by מאור ידין on 25/02/2020.
+//  Copyright © 2020 Around team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Product {
+     var productName : String
+     var productImage : UIImage
+     var productDesc : String
+    }
