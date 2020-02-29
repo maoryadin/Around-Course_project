ff44a23362d97c37cc0ce747dcccd49c69f71d37
Maor Yadin <maoryadin0888@gmail.com>
2020-02-29 16:48:43 +0200

    some changes to sing in view controller

--- Around/SignInViewController.swift
+++ Around/SignInViewController.swift
@@ -1,8 +1,15 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import FirebaseAuth
+import FirebaseDatabase
+
 
 class SignInViewController: UIViewController {
+
+    
+    var db:Firestore!
+    var userData:UserData!
     
     @IBOutlet weak var emailTF: UITextField!
     
@@ -11,31 +18,72 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+         let settings = FirestoreSettings()
+
+        Firestore.firestore().settings = settings
+          // [END setup]
+        db = Firestore.firestore()
         
-
-
+        
     }
     
     
     @IBAction func loginButton_Click(_ sender: Any) {
         
-
         FireBaseManager.Login(email: emailTF.text!, password: passwordTF.text!) { (success:Bool) in
             
             if(success){
-                print(success)
-            self.performSegue(withIdentifier: "showProfileLogIn", sender: sender)
-            }
-            
-            self.shouldPerformSegue(withIdentifier: "showProfileLogIn", sender: sender)
+                print("success login")
+                self.setData()
 
+            } else {
+                self.shouldPerformSegue(withIdentifier: "showProfileLogIn", sender: self)
+            }
         }
+        
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        print("before perpare")
+        if segue.identifier  == "showProfileLogIn" {
+            
+            print("we in segue destination to profile!")
+            
+        }
+        print("after perpare")
+
+        }
+    
+    func setData() {
+        print("befor set data")
+        let uid = Auth.auth().currentUser?.uid
+         print(uid!)
+        let docRef = self.db.collection("Users").document(uid!)
+                 docRef.getDocument { (document, error) in
+                     if let document = document, document.exists {
+                         let dataDescription = document.data()!
+
+                       for(key,value) in dataDescription.enumerated() {
+                           print("\(key):\(value)")
+                       }
+                       
+                        self.performSegue(withIdentifier: "showProfileLogIn", sender: self)
+            }
+         
+        }
+        
+        print("after set data")
+}
+//
+
+        
+
     
     @IBAction func createAccountButton_Click(_ sender: Any) {
         
-        print("!!!!!")
+ //       print("!!!!!")
         performSegue(withIdentifier: "registerSegue", sender: self)
 //
 //        FireBaseManager.CreateAccount(email: emailTF.text!, password: passwordTF.text!) {
@@ -50,6 +98,8 @@ class SignInViewController: UIViewController {
     
 
     override func shouldPerformSegue(withIdentifier: String, sender: Any?) -> Bool {
+        
+        print("going to cancel")
 
         if(withIdentifier == "showProfileLogIn") {
             return false
