c69360b96c9dfc5e21de1c93994db8b7194e7992
Ron <moreroron@gmail.com>
2020-04-21 17:16:08 +0300

    fix edit post

--- Around/addPostViewController.swift
+++ Around/addPostViewController.swift
@@ -28,7 +28,7 @@ class addPostViewController: UIViewController {
     let locationManager = CLLocationManager()
     var imagePicker:UIImagePickerController!
     var activityView:UIActivityIndicatorView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +38,7 @@ class addPostViewController: UIViewController {
         postBtn.setTitleColor(.white, for: .disabled)
         
         self.contentTextField.addDoneButton(title: "Done", target: self, selector: #selector(tapDone(sender:)))
-
+        
         // add image
         imageView.layer.borderColor = UIColor.init(hexString: "EEEFF7").cgColor
         imageView.layer.borderWidth = 1
@@ -53,19 +53,19 @@ class addPostViewController: UIViewController {
         postBtn.layer.cornerRadius =  postBtn.frame.size.height/2
         postBtn.layer.masksToBounds = true
         let imageTap = UITapGestureRecognizer(target:self,action: #selector(openImagePicker))
-
-          
-            //imageView.isUserInteractionEnabled = true
-          imageView.addGestureRecognizer(imageTap)
+        
+        
+        //imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(imageTap)
         addImageBtn.addGestureRecognizer(imageTap)
-          //imageView.layer.cornerRadius = imageView.bounds.height / 2
-          imageView.clipsToBounds = true
-
-                  
-          imagePicker = UIImagePickerController()
-            imagePicker.allowsEditing = true;
-          imagePicker.sourceType = .photoLibrary
-          imagePicker.delegate = self
+        //imageView.layer.cornerRadius = imageView.bounds.height / 2
+        imageView.clipsToBounds = true
+        
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true;
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
         
         if postCell != nil{
             updatePost(post: postCell!)
@@ -82,112 +82,104 @@ class addPostViewController: UIViewController {
         let ref:StorageReference? = FireBaseManager.getRef(path: self.post?.imageRef)
         FireBaseManager.getImageFromStorage(ref: ref!){
             image in
-        
             self.imageView.image = image
-            
         }
-
         
+        self.postBtn.isEnabled = true
+        self.postBtn.backgroundColor = .systemBlue
+        self.noteLabel.text = ""
+        self.addImageBtn.setImage(UIImage(systemName: "arrow.2.circlepath"), for: .normal)
     }
     
-
     @objc func openImagePicker(_ sender:Any) {
         //locationManager.startUpdatingLocation()
         self.present(imagePicker,animated: true,completion: nil)
-        
-      }
-    
-
-    
+    }
     
     func createNewPost() {
         let toPost:Post? = Post(uid: FireBaseManager.currentUser!.uid, username: FireBaseManager.user!.username, text: "", imageRef: "", time: "", lat: 0, long: 0)
-     let loc =  LocationService.sharedInstance.locationManager.location
-                let lat = loc!.coordinate.latitude
-                let long = loc!.coordinate.longitude
-                let currentDate = NSDate.now
-                toPost?.text = "\(contentTextField.text!)"
-                toPost?.username = FireBaseManager.user!.username
-                toPost?.time = currentDate.timeIntervalSince1970.description
-                toPost?.imageRef = ("account/\(toPost!.uid)/posts/\(toPost!.time)/postImage.jpeg")
-                toPost?.lat = lat
-                toPost?.long = long
-                        if let data = self.imageView.image?.jpegData(compressionQuality: 0.75) {
+        let loc =  LocationService.sharedInstance.locationManager.location
+        let lat = loc!.coordinate.latitude
+        let long = loc!.coordinate.longitude
+        let currentDate = NSDate.now
+        toPost?.text = "\(contentTextField.text!)"
+        toPost?.username = FireBaseManager.user!.username
+        toPost?.time = currentDate.timeIntervalSince1970.description
+        toPost?.imageRef = ("account/\(toPost!.uid)/posts/\(toPost!.time)/postImage.jpeg")
+        toPost?.lat = lat
+        toPost?.long = long
+        if let data = self.imageView.image?.jpegData(compressionQuality: 0.75) {
+            
+            print("profile image is compressed")
+            let ref = FireBaseManager.getRef(path: toPost?.imageRef)
+            FireBaseManager.uploadFile(data: data, ref: ref,completion: {
+                
+                FireBaseManager.db.collection("Posts").document("\(toPost!.uid)_\(toPost!.time)").setData((toPost!.toJson()), merge: false, completion:{ error in
+                    DispatchQueue.main.async {
+                        
+                        MBProgressHUD.hide(for: UIApplication.shared.keyWindow!, animated: true)
+                        self.dismiss(animated: true, completion: nil)
+                        
+                    }
                     
-                            print("profile image is compressed")
-                            let ref = FireBaseManager.getRef(path: toPost?.imageRef)
-                            FireBaseManager.uploadFile(data: data, ref: ref,completion: {
-                                
-                                FireBaseManager.db.collection("Posts").document("\(toPost!.uid)_\(toPost!.time)").setData((toPost!.toJson()), merge: false, completion:{ error in
-                                    DispatchQueue.main.async {
-
-                                        MBProgressHUD.hide(for: UIApplication.shared.keyWindow!, animated: true)
-                                        self.dismiss(animated: true, completion: nil)
-
-                                    }
-                                    
-                                })
-        }
-                            )}
+                })
+            }
+            )}
         
-                  print("collection is created")
+        print("collection is created")
     }
-
-     @IBAction func PostActionButtom_clicked(_ sender: Any) {
+    
+    @IBAction func PostActionButtom_clicked(_ sender: Any) {
         DispatchQueue.main.async {
-
+            
             MBProgressHUD.showAdded(to: UIApplication.shared.keyWindow!, animated: true)
         }
         if post == nil{
             createNewPost()
             return
         }
-
+        
         
         let loc =  LocationService.sharedInstance.locationManager.location
         let lat = loc!.coordinate.latitude
         let long = loc!.coordinate.longitude
-
- //       let currentDate = NSDate.now
+        
+        //       let currentDate = NSDate.now
         post?.text = "\(contentTextField.text!)"
         post?.username = FireBaseManager.user!.username
-//        post?.time = currentDate.timeIntervalSince1970.description
+        //        post?.time = currentDate.timeIntervalSince1970.description
         post?.imageRef = ("account/\(post!.uid)/posts/\(post!.time)/postImage.jpeg")
         post?.lat = lat
         post?.long = long
-                if let data = self.imageView.image?.jpegData(compressionQuality: 0.75) {
+        if let data = self.imageView.image?.jpegData(compressionQuality: 0.75) {
             
-                    print("profile image is compressed")
-                    let ref = FireBaseManager.getRef(path: post?.imageRef)
-                    FireBaseManager.uploadFile(data: data, ref: ref,completion: {
-                        
-
-                        FireBaseManager.db.collection("Posts").document("\(self.post!.uid)_\(self.post!.time)").updateData(self.post!.toJson(), completion: {(error) in
-                            if error == nil {
-                                    print("updated successfully")
-                                Post.updateByTime(post: self.post!)
-                                self.from?.filterList()
-                                DispatchQueue.main.async {
-
-                                    MBProgressHUD.hide(for: UIApplication.shared.keyWindow!, animated: true)
-                                    self.dismiss(animated: true, completion: nil)
-
-                                }
-                            }
-                            else{
-                                print("error updating collection")
-                            }
-                        })
-})}
-                    print("collection is updated")
-        }
-        
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-
+            print("profile image is compressed")
+            let ref = FireBaseManager.getRef(path: post?.imageRef)
+            FireBaseManager.uploadFile(data: data, ref: ref,completion: {
+                
+                
+                FireBaseManager.db.collection("Posts").document("\(self.post!.uid)_\(self.post!.time)").updateData(self.post!.toJson(), completion: {(error) in
+                    if error == nil {
+                        print("updated successfully")
+                        Post.updateByTime(post: self.post!)
+                        self.from?.filterList()
+                        DispatchQueue.main.async {
+                            
+                            MBProgressHUD.hide(for: UIApplication.shared.keyWindow!, animated: true)
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                    else {
+                        print("error updating collection")
+                    }
+                })
+            })}
+        print("collection is updated")
     }
-
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
+    
 }
 
 extension addPostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -213,19 +205,18 @@ extension addPostViewController: UIImagePickerControllerDelegate, UINavigationCo
             self.postBtn.backgroundColor = .systemBlue
             self.noteLabel.text = ""
             self.addImageBtn.setImage(UIImage(systemName: "arrow.2.circlepath"), for: .normal)
-            
         }
         
         if (self.imageView.image?.jpegData(compressionQuality: 0.75)) != nil {
             
-//            let ref = FireBaseManager.getRef(path: userData?.profilePicRef)
-////            FireBaseManager.uploadFile(data: data, ref: ref, completion: getDocumentAndSetData)
-//
-//            FireBaseManager.uploadFile(data: data, ref: ref,completion: getDocumentAndSetData)
-//
-//
-//
-                
-            }
+            //            let ref = FireBaseManager.getRef(path: userData?.profilePicRef)
+            ////            FireBaseManager.uploadFile(data: data, ref: ref, completion: getDocumentAndSetData)
+            //
+            //            FireBaseManager.uploadFile(data: data, ref: ref,completion: getDocumentAndSetData)
+            //
+            //
+            //
+            
         }
+    }
 }
